ba821c2d6f445b3e2658cb638661e5a48600e67b
Omniconverge-Technik <accounts@omniconverge.com>
2024-12-08 16:23:46 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "2dd0eb6864f759e5abcee9dadb853706",
+"flutter_bootstrap.js": "5cfa4595480b1b0c2965c511432944f6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "99a2c564518e0f9076ab30fe75cb84d3",
 "/": "99a2c564518e0f9076ab30fe75cb84d3",
-"main.dart.js": "47c66ebe8d51a33d40157d0d53e4beb6",
+"main.dart.js": "c1931a0ee02b64939aec2276445339d1",
 "manifest.json": "f8b402c32657f9a3a82f122042ef5d37",
 "version.json": "8ca8f30ad979d93da0f02b24a560832d"};
 // The application shell files that are downloaded before a service worker can
